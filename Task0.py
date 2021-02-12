72636d2e60e42bc78a39dce57f333b7a53d9cd03
Batmanghelich, Farhad <feb.lich@gmail.com>
2021-02-12 07:04:48 -0800

    Task0 first attempt

--- Task0.py
+++ Task0.py
@@ -11,7 +11,6 @@ with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
-
 """
 TASK 0:
 What is the first record of texts and what is the last record of calls?
@@ -20,3 +19,5 @@ Print messages:
 "Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
 """
 
+print('First record of texts, {} texts {} at time {}'.format(texts[0][0], texts[0][1], texts[0][2]))
+print('Last record of calls, {} calls {} at time {}, lasting {} seconds'.format(calls[-1][0], calls[-1][1], calls[-1][2], calls[-1][3]))
