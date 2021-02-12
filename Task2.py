714691c20a75859d8c945964fef617421e4f73ba
Batmanghelich, Farhad <feb.lich@gmail.com>
2021-02-12 08:58:14 -0800

    Task2 first attempt

--- Task2.py
+++ Task2.py
@@ -19,4 +19,16 @@ Print a message:
 "<telephone number> spent the longest time, <total time> seconds, on the phone during 
 September 2016.".
 """
+from collections import defaultdict
+from operator import itemgetter
+d = defaultdict(int)
+for record in calls:
+    d[record[0]] += (int(record[3]))
+    d[record[1]] += (int(record[3]))
 
+# convert d from defaultdict to d to use operator.itemgetter module
+d = dict(d)
+maxNumber = max(d.items(), key=itemgetter(1))[0]
+maxDuration = max(d.items(), key=itemgetter(1))[1]
+
+print('{} spent the longest time, {} seconds, on the phone during September 2016.'.format(maxNumber, maxDuration))
