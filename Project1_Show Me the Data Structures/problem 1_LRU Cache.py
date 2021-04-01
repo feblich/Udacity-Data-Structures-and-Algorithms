076f4327eb18e1211bacf2f441412ce044d6b4e5
Batmanghelich, Farhad <feb.lich@gmail.com>
2021-04-01 08:06:38 -0700

    problem 1 first working implementation

--- Project1_Show Me the Data Structures/problem 1_LRU Cache.py
+++ Project1_Show Me the Data Structures/problem 1_LRU Cache.py
@@ -1,1 +1,25 @@
-import numpy as np
+
+from collections import OrderedDict
+class LRUCache:
+
+    def __init__(self, capacity):
+
+        self.capacity = capacity
+        self.ordereddict = OrderedDict()
+
+    def get(self, key):
+
+        if key not in self.ordereddict:
+            return -1
+
+        self.ordereddict.move_to_end(key)
+        return self.ordereddict[key]
+
+    def set(self, key, value):
+
+        if key in self.ordereddict:
+            self.ordereddict.move_to_end(key)
+        self.ordereddict[key] = value
+
+        if len(self.ordereddict) > self.capacity:
+            self.ordereddict.popitem(last=False)
