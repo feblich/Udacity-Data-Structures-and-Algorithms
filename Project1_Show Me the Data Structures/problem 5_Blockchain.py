44e9d0e7e016aaacaeba2dc528bf6b8a3b31ad58
Batmanghelich, Farhad <feb.lich@gmail.com>
2021-04-25 15:58:57 -0700

    added block location and test cases to p5

--- Project1_Show Me the Data Structures/problem 5_Blockchain.py
+++ Project1_Show Me the Data Structures/problem 5_Blockchain.py
@@ -13,8 +13,8 @@ class Block:
         sha.update(hash_str.encode('utf-8'))
         return sha.hexdigest()
 
-class Blockchain:
 
+class Blockchain:
     def __init__(self):
         self.tail = None
 
@@ -36,4 +36,42 @@ class Blockchain:
 
         new_block = Block(data=data, previous_hash=None)
         self.tail = new_block
+
+    def get_block_location(self, data):
+
+        if self.tail.data == data:
+            location = self.size()
+            return location, data
+
+        current_tail = self.tail
+        current_loc = self.size()
+        while current_tail:
+            if current_tail.data != data:
+                current_loc -= 1
+                if current_loc == 0:
+                    return None
+                current_tail = current_tail.previous_hash
+            else:
+                return current_loc, data
+
+
+# testing
+if __name__ == '__main__':
+
+    blockchain = Blockchain()
+    print(blockchain.size())  # must be 0
+
+    blockchain.append('data0')
+    blockchain.append('data1')
+    blockchain.append('data2')
+    blockchain.append('data3')
+
+    print(blockchain.size())  # must be 4
+    print(blockchain.tail.data)  # must be 'data3'
+    print(blockchain.get_block_location('data1'))  # must return (2, 'data1') which is location of this block
+
+
+
+
+
 
