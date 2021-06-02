c0fa0f66122098b922ac1cfba4a38d3af0353921
Batmanghelich, Farhad <feb.lich@gmail.com>
2021-06-02 09:17:54 -0700

    added test cases to problem 3

--- Project1_Show Me the Data Structures/problem 3_Huffman Coding.py
+++ Project1_Show Me the Data Structures/problem 3_Huffman Coding.py
@@ -12,7 +12,9 @@ class Node(object):
         self.freq = freq
         self.huff_binary = ''
 
+
 def get_huffman_tree(the_string):
+
     d = {}
     letters = ''.join(set(the_string))
     for l in letters:
@@ -23,7 +25,8 @@ def get_huffman_tree(the_string):
 
     while len(nodes) > 1:
         nodes = sorted(nodes, key=lambda
-            x: x.freq)  # sort nodes based on freq so index 0 and 1 can be chosen as left and right child
+            x: (x.freq, x.letter))  # sort nodes based on freq so index 0 and 1 can be chosen as left and right child
+
         left_child = nodes[0]
         right_child = nodes[1]
         left_child.huff_binary = 0
@@ -35,6 +38,7 @@ def get_huffman_tree(the_string):
         nodes.append(new_node)
 
     return nodes[0]
+
 
 def traverse_huffman_tree(huffman_tree, val='', letter_codes=defaultdict(list)):
 
@@ -49,7 +53,10 @@ def traverse_huffman_tree(huffman_tree, val='', letter_codes=defaultdict(list)):
 
     return letter_codes
 
+
 def huffman_encoding(data):
+    if not data:
+        raise ValueError("input sentence should not be empty")
     huffman_tree = get_huffman_tree(data)
     huff_codes_dict = traverse_huffman_tree(huffman_tree)
     encoded_data = ''
@@ -59,13 +66,98 @@ def huffman_encoding(data):
     return encoded_data, huffman_tree
 
 
+def huffman_decoding(encoded_data,tree):
+
+    decoded_data = ''
+    curr_tree = tree
+    for bit in encoded_data:
+
+        if bit == str(0):
+            curr_tree = curr_tree.left_child
+
+        if bit == str(1):
+            curr_tree = curr_tree.right_child
+
+        if curr_tree.left_child is None and curr_tree.right_child is None:
+            decoded_data += curr_tree.letter
+            curr_tree = tree
+
+    return decoded_data
 
 
-
-def huffman_decoding(data,tree):
-    pass
 
 if __name__ == "__main__":
 
-    s = 'AAAAAAABBBCCCCCCCDDEEEEEE'
-    encoded_data, tree = huffman_encoding(s)
+    ## test with "AAAAAAABBBCCCCCCCDDEEEEEE"
+    s = "AAAAAAABBBCCCCCCCDDEEEEEE"
+    encoded_data, huffman_tree = huffman_encoding(s)
+    decoded_data = huffman_decoding(encoded_data, huffman_tree)
+    print(decoded_data)  # "AAAAAAABBBCCCCCCCDDEEEEEE"
+
+    ## test with another sentence "The bird is the word"
+    a_great_sentence = "The bird is the word"
+
+    print("The size of the data is: {}\n".format(sys.getsizeof(a_great_sentence)))
+    # The size of the data is: 69
+    print("The content of the data is: {}\n".format(a_great_sentence))
+    # The content of the data is: The bird is the word
+
+    encoded_data, tree = huffman_encoding(a_great_sentence)
+
+    print("The size of the encoded data is: {}\n".format(sys.getsizeof(int(encoded_data, base=2))))
+    # The size of the encoded data is: 36
+    print("The content of the encoded data is: {}\n".format(encoded_data))
+    # The content of the encoded data is: 1110000100011011101010100111111001001111101010001000110101101101001111
+
+    decoded_data = huffman_decoding(encoded_data, tree)
+
+    print("The size of the decoded data is: {}\n".format(sys.getsizeof(decoded_data)))
+    # The size of the decoded data is: 69
+    print("The content of the encoded data is: {}\n".format(decoded_data))
+    # The content of the encoded data is: The bird is the word
+
+
+    ## edge case 1 (multiple spaces) "Sentence with     multiple spaces"
+    multiple_space_sentence = "Sentence with     multiple spaces"
+    print("The size of the data is: {}\n".format(sys.getsizeof(multiple_space_sentence)))
+    # The size of the data is: 82
+    print("The content of the data is: {}\n".format(multiple_space_sentence))
+    # The content of the data is: Sentence with     multiple spaces
+
+    encoded_data, tree = huffman_encoding(multiple_space_sentence)
+
+    print("The size of the encoded data is: {}\n".format(sys.getsizeof(int(encoded_data, base=2))))
+    # The size of the encoded data is: 40
+    print("The content of the encoded data is: {}\n".format(encoded_data))
+    # The content of the encoded data is: 01000110100111111101001010111000111010111111101100000000000001101111001000111101111010100011000101110100100101011101011
+
+    decoded_data = huffman_decoding(encoded_data, tree)
+
+    print("The size of the decoded data is: {}\n".format(sys.getsizeof(decoded_data)))
+    # The size of the decoded data is: 82
+    print("The content of the encoded data is: {}\n".format(decoded_data))
+    # The content of the encoded data is: Sentence with     multiple spaces
+
+
+    ## edge case 2 (empty sentence) ""
+    empty_sentence = ""
+    print("The size of the data is: {}\n".format(sys.getsizeof(empty_sentence)))
+    # The size of the data is: 51
+    print("The content of the data is: {}\n".format(empty_sentence))
+    # The content of the data is:
+    try:
+        encoded_data, tree = huffman_encoding(empty_sentence)
+        print("The size of the encoded data is: {}\n".format(sys.getsizeof(int(encoded_data, base=2))))
+        print("The content of the encoded data is: {}\n".format(encoded_data))
+
+        decoded_data = huffman_decoding(encoded_data, tree)
+
+        print("The size of the decoded data is: {}\n".format(sys.getsizeof(decoded_data)))
+        print("The content of the encoded data is: {}\n".format(decoded_data))
+    except ValueError as v:
+        print(v)
+        # input sentence should not be empty
+
+
+
+
